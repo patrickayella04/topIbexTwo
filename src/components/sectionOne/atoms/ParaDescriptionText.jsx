e943beabbdb3944a572e397546d73d0faae42fbd
Patrick Nyeko <patricknyeko@Patricks-MacBook-Air.local>
2021-05-14 21:56:20 +0100

    box size

--- src/components/sectionOne/atoms/ParaDescriptionText.jsx
+++ src/components/sectionOne/atoms/ParaDescriptionText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ParaDescriptionText() {
   return (
     <p className="para-des-text">
-      Maximise the growth of your business <br /> with its own bespoke website
+      Maximize the growth of your business <br /> with its own bespoke website
       designed for the future.
     </p>
   );
