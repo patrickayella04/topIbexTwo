21c0ac13c8f824224f6e92f24d32936f195f9327
Patrick Nyeko <patricknyeko@Patricks-MacBook-Air.local>
2021-05-20 01:57:49 +0100

    solid

--- src/components/sectionFive/atoms/LittleHeaderText.jsx
+++ src/components/sectionFive/atoms/LittleHeaderText.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function LittleHeaderText() {
-  return <h3>Essential</h3>;
+  return <h3 className="five-little-header">Essential</h3>;
 }
 
 export default LittleHeaderText;
