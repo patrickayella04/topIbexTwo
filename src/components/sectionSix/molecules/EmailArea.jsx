03e9636d8f2eb3361e00a502fab242fd22ac6754
Patrick Nyeko <patricknyeko@Patricks-MacBook-Air.local>
2021-06-16 03:03:24 +0100

    input update and form validation on change

--- src/components/sectionSix/molecules/EmailArea.jsx
+++ src/components/sectionSix/molecules/EmailArea.jsx
@@ -6,25 +6,31 @@ import emailjs from "emailjs-com";
 
 function EmailArea() {
   const Form = () => {
+    const [input, setInput] = useState(false);
+
     function sendEmail(e) {
       e.preventDefault();
 
-      emailjs
-        .sendForm(
-          "service_a374dk8",
-          "template_77mr41i",
-          e.target,
-          "user_ubGTGQUa1rTyqnUHX798m"
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-      e.target.reset();
+      if (input === true) {
+        emailjs
+          .sendForm(
+            "service_a374dk8",
+            "template_77mr41i",
+            e.target,
+            "user_ubGTGQUa1rTyqnUHX798m"
+          )
+          .then(
+            (result) => {
+              console.log(result.text);
+            },
+            (error) => {
+              console.log(error.text);
+            }
+          );
+        e.target.reset();
+      } else {
+        alert("Please Enter Correct Information");
+      }
     }
 
     return (
@@ -38,26 +44,53 @@ function EmailArea() {
           <div className="form-wrapper">
             <p className="name-para input-para">
               {/* <label className="name-lable input-lable">Name</label> */}
-              <input type="text" name="from_name" placeholder="Name" />
+              <input
+                type="text"
+                name="from_name"
+                placeholder="Name"
+                onChange={() => setInput(true)}
+              />
             </p>
             <p className="email-para input-para">
               {/* <label className="email-lable input-lable">Email Address</label> */}
-              <input type="email" name="reply_to" placeholder="Email Address" />
+              <input
+                type="email"
+                name="reply_to"
+                placeholder="Email Address"
+                onChange={() => setInput(true)}
+              />
             </p>
             <p className="company-para input-para">
               {/* <label className="company-lable input-lable">Company</label> */}
-              <input type="text" name="from_company" placeholder="Company" />
+              <input
+                type="text"
+                name="from_company"
+                placeholder="Company"
+                onChange={() => setInput(true)}
+              />
             </p>
 
             <p className="phone-para input-para">
               {/* <label className="phone-lable input-lable">Phone Number</label> */}
-              <input type="text" name="from_phone" placeholder="Phone" />
+              <input
+                type="text"
+                name="from_phone"
+                placeholder="Phone"
+                onChange={() => setInput(true)}
+              />
             </p>
           </div>
+
           <p className="message-para input-para">
             {/* <label className="message-lable input-lable">Message</label> */}
-            <textarea name="message" rows="5" placeholder="Message"></textarea>
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="Message"
+              onChange={() => setInput(true)}
+            ></textarea>
           </p>
+
           <p className="submit-para input-para">
             <button className="email-submit" type="submit">
               Submit
@@ -76,76 +109,3 @@ function EmailArea() {
 }
 
 export default EmailArea;
-
-// const [email, setEmail] = useState("");
-// // const [message, setMessage] = useState("");
-// // const [name, setName] = useState("");
-// // const [number, setNumber] = useState("");
-// // const [company, setCompany] = useState("");
-
-// // const [result, setResult] = useState(null);
-
-// // const handleRequest = async (e) => {
-// //   e.preventDefault();
-// //   console.log({ email, message, name, number, company });
-// //   const body = {
-// //     email,
-// //     message,
-// //     number,
-// //     name,
-// //     company,
-// //   };
-// //   await axios
-// //     .post("/send", body, {
-// //       headers: {
-// //         "Content-type": "application/json",
-// //       },
-// //     })
-// //     .then((res) => {
-// //       alert("Email Sent Succesfully");
-// //       console.log(res);
-// //     })
-// //     .catch((err) => {
-// //       console.log(err);
-// //     });
-// // };
-// const [state, setState] = useState({
-//   name: "",
-//   email: "",
-//   company: "",
-//   message: "",
-//   number: "",
-// });
-
-// const [result, setResult] = useState(null);
-
-// const sendEmail = (event) => {
-//   event.preventDefault();
-//   axios
-//     .post("/send", { ...state })
-//     .then((response) => {
-//       setResult(response.data);
-//       setState({
-//         name: "",
-//         email: "",
-//         company: "",
-//         message: "",
-//         phone: "",
-//       });
-//     })
-//     .catch(() => {
-//       setResult({
-//         success: false,
-//         message: "Something went wrong. Try again later",
-//       });
-//     });
-// };
-
-// const onInputChange = (event) => {
-//   const { name, value } = event.target;
-
-//   setState({
-//     ...state,
-//     [name]: value,
-//   });
-// };
